670016554ccf6d62fa59d4ee49312231ba101418
Daniel Tulpa <dtulpa16@yahoo.com>
2023-10-11 09:56:33 -0600

    Mod

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useAuth } from "../context/AuthContext";
 
 const HomePage = () => {
   const { token, user } = useAuth();
 
+  //* Below is an example of how you will make axios requests to a protected endpoint
+
+  // useEffect(()=>{
+  //   getItems()
+  // },[])
+  // async function getItems() {
+  //   let response = await axios.get("http://url-to-your-endpoint/", {
+  //     headers: {
+  //       "x-auth-token": token,
+  //     },
+  //   });
+  // }
+
   //Open the console and see what "token" & and the decoded "user" look like!
-  console.log("User: ",user);
-  console.log("Token: ",token);
+  console.log("User: ", user);
+  console.log("Token: ", token);
   return (
     <div>
       <h2>Welcome {user?.name}</h2>
