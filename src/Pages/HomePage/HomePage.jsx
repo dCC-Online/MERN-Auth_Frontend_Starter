1bb58ea4695f9daf737714385b92276c18f7c864
Daniel Tulpa <dtulpa16@yahoo.com>
2024-02-02 16:23:37 -0600

    Added Example Of Loader Function With Protected Endpoint Request

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useLoaderData } from 'react-router-dom';
+import axios from 'axios';
+
+//* Example of loader function with auth API request
+export async function getCars() {
+  let token = localStorage.getItem("token");
+  let response = await axios.get(`http://localhost:5000/api/cars`, {
+    headers: { Authorization: "Bearer " + token },
+  });
+  return response.data;
+}
+
 
 export default function HomePage() {
+  const data = useLoaderData()
+  const [cars,setCars] = useState(data)
+
   return (
     <div>HomePage</div>
   )
